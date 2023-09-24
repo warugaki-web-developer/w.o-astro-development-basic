db0bd3b661ca84ceebedab5a73720d9ca430a1fd
warugaki <warugaki.web.developer@gmail.com>
2023-09-24 18:44:56 +0900

    prettierのインストールと設定

---
+++ .prettierrc.cjs
@@ -0,0 +1,24 @@
+/** @type {import("prettier").Config} */
+
+module.exports = {
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+    {
+      files: ['**/*.css', '**/*.scss'],
+      options: {
+        singleQuote: false,
+      },
+    },
+  ],
+};
